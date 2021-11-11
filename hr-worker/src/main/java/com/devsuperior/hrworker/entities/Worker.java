65dfdc7cc548848b7bd8c465432086a5926f06d5
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2021-11-10 22:43:07 -0300

    Implementando projeto hr-worker

--- hr-worker/src/main/java/com/devsuperior/hrworker/entities/Worker.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/entities/Worker.java
@@ -3,12 +3,20 @@ package com.devsuperior.hrworker.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-
-public class Worker implements Serializable{
+@Entity
+@Table(name = "tb_worker")
+public class Worker implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Double dailyIncome;
