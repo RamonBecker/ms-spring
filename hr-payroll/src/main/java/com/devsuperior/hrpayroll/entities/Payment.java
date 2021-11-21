fb661b3acacc362e88833817698c11378e67fcc0
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2021-11-21 12:18:10 -0300

    hr-worker como cliente do servidor de configuração

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Payment.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Payment.java
@@ -1,20 +1,19 @@
 package com.devsuperior.hrpayroll.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 public class Payment implements Serializable {
-
 	private static final long serialVersionUID = 1L;
 
 	private String name;
 	private Double dailyIncome;
 	private Integer days;
-
+	
 	public Payment() {
 	}
 
 	public Payment(String name, Double dailyIncome, Integer days) {
+		super();
 		this.name = name;
 		this.dailyIncome = dailyIncome;
 		this.days = days;
@@ -43,27 +42,8 @@ public class Payment implements Serializable {
 	public void setDays(Integer days) {
 		this.days = days;
 	}
-
+	
 	public double getTotal() {
-		return getDays() * getDailyIncome();
+		return days * dailyIncome;
 	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(dailyIncome, days, name);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Payment other = (Payment) obj;
-		return Objects.equals(dailyIncome, other.dailyIncome) && Objects.equals(days, other.days)
-				&& Objects.equals(name, other.name);
-	}
-
 }
--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,32 +14,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.hrworker.entities.Worker;
-import com.devsuperior.hrworker.repositories.WorkerRepositoy;
+import com.devsuperior.hrworker.repositories.WorkerRepository;
 
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
-
+	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
+	
 	@Autowired
 	private Environment env;
-
+	
 	@Autowired
-	private WorkerRepositoy repository;
-
+	private WorkerRepository repository;
+	
+		
+	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
 		List<Worker> list = repository.findAll();
 		return ResponseEntity.ok(list);
-	}
-
+	}	
+	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
-
-
+		
+		try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
-		Worker wk = repository.findById(id).get();
-		return ResponseEntity.ok(wk);
-	}
+		
+		Worker obj = repository.findById(id).get();
+		return ResponseEntity.ok(obj);
+	}	
 }
